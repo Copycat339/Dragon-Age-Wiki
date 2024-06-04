a0e85aa66eb9d32c43bf7d04dfb00025c0e4ac9a
Copycat339 <161411275+Copycat339@users.noreply.github.com>
2024-06-03 22:54:36 -0600

    Wiki 3er intento

--- code/js/busca.js
+++ code/js/busca.js
@@ -3,26 +3,26 @@ document.addEventListener('DOMContentLoaded', function () {
   const suggestions = document.getElementById('suggestions');
 
   const items = [
-    { name: 'Alistair', link: '/Alistair'},
-    { name: 'Andraste', link: '/Andraste' },
-    { name: 'Anora', link: '/Anora' },
-    { name: 'Cailan', link: '/Cailan' },
-    { name: 'Cassandra', link: '/Cassandra' },
-    { name: 'El Hacedor', link: '/El-Hacedor' },
-    { name: 'Flemeth', link: '/Flemeth' },
-    { name: 'Florianne', link: '/Florianne' },
-    { name: 'Hawke', link: '/Hawke' },
-    { name: 'Isabela', link: '/Isabela' },
-    { name: 'Leliana', link: '/Leliana' },
-    { name: 'Loghain', link: '/Loghain' },
-    { name: 'Meredith', link: '/Meredith' },
-    { name: 'Merrill', link: '/Merrill' },
-    { name: 'Morrigan', link: '/Morrigan' },
-    { name: 'Sera', link: '/Sera' },
-    { name: 'Solas', link: '/Solas' },
-    { name: 'The Inquisitor', link: '/The-Inquisitor' },
-    { name: 'The Warden', link: '/The-Warden' },
-    { name: 'Varric', link: '/Varric' }
+    { name: 'Alistair', link: 'https://copycat339.github.io/Dragon-Age-Wiki/Alistair'},
+    { name: 'Andraste', link: 'https://copycat339.github.io/Dragon-Age-Wiki/Andraste' },
+    { name: 'Anora', link: 'https://copycat339.github.io/Dragon-Age-Wiki/Anora' },
+    { name: 'Cailan', link: 'https://copycat339.github.io/Dragon-Age-Wiki/Cailan' },
+    { name: 'Cassandra', link: 'https://copycat339.github.io/Dragon-Age-Wiki/Cassandra' },
+    { name: 'El Hacedor', link: 'https://copycat339.github.io/Dragon-Age-Wiki/El-Hacedor' },
+    { name: 'Flemeth', link: 'https://copycat339.github.io/Dragon-Age-Wiki/Flemeth' },
+    { name: 'Florianne', link: 'https://copycat339.github.io/Dragon-Age-Wiki/Florianne' },
+    { name: 'Hawke', link: 'https://copycat339.github.io/Dragon-Age-Wiki/Hawke' },
+    { name: 'Isabela', link: 'https://copycat339.github.io/Dragon-Age-Wiki/Isabela' },
+    { name: 'Leliana', link: 'https://copycat339.github.io/Dragon-Age-Wiki/Leliana' },
+    { name: 'Loghain', link: 'https://copycat339.github.io/Dragon-Age-Wiki/Loghain' },
+    { name: 'Meredith', link: 'https://copycat339.github.io/Dragon-Age-Wiki/Meredith' },
+    { name: 'Merrill', link: 'https://copycat339.github.io/Dragon-Age-Wiki/Merrill' },
+    { name: 'Morrigan', link: 'https://copycat339.github.io/Dragon-Age-Wiki/Morrigan' },
+    { name: 'Sera', link: 'https://copycat339.github.io/Dragon-Age-Wiki/Sera' },
+    { name: 'Solas', link: 'https://copycat339.github.io/Dragon-Age-Wiki/Solas' },
+    { name: 'The Inquisitor', link: 'https://copycat339.github.io/Dragon-Age-Wiki/The-Inquisitor' },
+    { name: 'The Warden', link: 'https://copycat339.github.io/Dragon-Age-Wiki/The-Warden' },
+    { name: 'Varric', link: 'https://copycat339.github.io/Dragon-Age-Wiki/Varric' }
     // Agrega más elementos aquí
   ];
 
